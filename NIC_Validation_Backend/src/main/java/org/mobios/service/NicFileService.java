3e5c4fcce8e5981f77ea3adc31634425fe567113
ushalka kapugamage <ushalkakapugamage@gmail.com>
2024-01-22 17:18:43 +0530

    NIC Validation stage 01

--- NIC_Validation_Backend/src/main/java/org/mobios/service/NicFileService.java
+++ NIC_Validation_Backend/src/main/java/org/mobios/service/NicFileService.java
@@ -1,11 +1,24 @@
 package org.mobios.service;
 
 
+import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface NicFileService {
+    void addNICFile(MultipartFile files);
+    List<String[]> getAllNICFiles();
+
+    List<String[]> parseCSV(MultipartFile file) throws IOException, CsvValidationException;
+
+    void processNICFiles(List<String[]> files);
+
+    void validateNIC(String nic);
+
+
 
 
 }
--- NIC_Validation_Backend/src/main/java/org/mobios/service/impl/NicFileServiceImpl.java
+++ NIC_Validation_Backend/src/main/java/org/mobios/service/impl/NicFileServiceImpl.java
@@ -1,8 +1,76 @@
 package org.mobios.service.impl;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import org.mobios.service.NicFileService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NicFileServiceImpl implements NicFileService{
+    @Override
+    public void addNICFile(MultipartFile files) {
+
+    }
+
+    @Override
+    public List<String[]> getAllNICFiles() {
+        return null;
+    }
+
+    @Override
+    public List<String[]> parseCSV(MultipartFile file) throws IOException, CsvValidationException {
+        if (file == null || (!file.getOriginalFilename().toLowerCase().endsWith(".csv") && (!file.getOriginalFilename().toLowerCase().endsWith(".txt")))){
+            throw new IllegalArgumentException("Invalid file type. Please provide a CSV or text file.");
+        }
+
+        Reader reader = new InputStreamReader(file.getInputStream());
+        CSVReader csvReader = new CSVReader(reader);
+        List<String[]> records = new ArrayList<>();
+        String[] record;
+        while ((record = csvReader.readNext()) != null){
+            records.add(record);
+        }
+        return records;
+    }
+
+    @Override
+    public void processNICFiles(List<String[]> files) {
+        for (String [] file : files){
+            if (file[0].length() != 12){
+                throw new IllegalArgumentException("Please check the NIC ="+file[0]);
+            }
+            validateNIC(file[0]);
+        }
+
+    }
+
+    @Override
+    public void validateNIC(String nic) {
+        LocalDate birthdate = null;
+        Period age;
+        String gender;
+
+        int temp = Integer.parseInt(nic.substring(4,7));
+        if (temp > 500){
+            gender = "Female";
+            temp-=500;
+        }else{
+            gender="Male";
+        }
+
+
+        LocalDate currentDate = LocalDate.now();
+        age = Period.between(birthdate,currentDate);
+
+
+    }
 }
